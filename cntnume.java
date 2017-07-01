7c1d015d0e0fff495c460b4a2c124bdf9405f442
mayuriyam <mayuriya21@gmail.com>
2017-07-01 12:02:26 +0530

    Create cntnume.java

---
+++ cntnume.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class cntnume
+{
+    public static void main(String ar[])
+    {
+        Scanner sc=new Scanner (System.in);
+       String a=sc.nextLine();
+       System.out.println(a.length());
+    }
+}
